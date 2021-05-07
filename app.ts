4572169ff9b220cc12de3370c160cfa661ac41a6
= <=>
2021-05-07 17:22:53 +0300

    wrapp server to airbrake

--- app.ts
+++ app.ts
@@ -13,40 +13,44 @@ import * as errorHandler from 'koa-better-error-handler';
 
 import { initPlanetsRoutes } from './src/routes';
 
-const app = new Koa();
-const router = new Router({
-  prefix: '/api/v1',
-});
+const startApp = (): void => {
+  const app = new Koa();
+  const router = new Router({
+    prefix: '/api/v1',
+  });
 
-new Notifier({
+  app.context.onerror = errorHandler;
+
+  app.use(cors());
+  app.use(koaBody());
+  app.use(logger());
+  app.use(
+    compress({
+      filter(contentType) {
+        return /text/i.test(contentType);
+      },
+      threshold: 2048,
+      gzip: {
+        flush: require('zlib').constants.Z_SYNC_FLUSH,
+      },
+      deflate: {
+        flush: require('zlib').constants.Z_SYNC_FLUSH,
+      },
+      br: false, // disable brotli
+    })
+  );
+  app.use(koa404Handler);
+  app.use(router.routes()).use(router.allowedMethods());
+
+  initPlanetsRoutes(router);
+
+  app.listen(process.env.PORT || 3001);
+};
+
+const airbrake = new Notifier({
   projectId: Number(process.env.AIRBRAKE_PROJECT_ID),
   projectKey: process.env.AIRBRAKE_PROJECT_KEY,
-  environment: 'production'
+  environment: 'production',
 });
 
-app.context.onerror = errorHandler;
-
-app.use(cors());
-app.use(koaBody());
-app.use(logger());
-app.use(
-  compress({
-    filter(contentType) {
-      return /text/i.test(contentType);
-    },
-    threshold: 2048,
-    gzip: {
-      flush: require('zlib').constants.Z_SYNC_FLUSH,
-    },
-    deflate: {
-      flush: require('zlib').constants.Z_SYNC_FLUSH,
-    },
-    br: false, // disable brotli
-  })
-);
-app.use(koa404Handler);
-app.use(router.routes()).use(router.allowedMethods());
-
-initPlanetsRoutes(router);
-
-app.listen(process.env.PORT || 3001);
+airbrake.call(startApp);
